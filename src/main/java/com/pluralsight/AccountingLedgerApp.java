d3c9a7685a229f94d75713ae8c144ef910fc2491
Djharness <djharness2@gmail.com>
2024-10-18 15:57:34 -0400

    checkpoint

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,26 +1,25 @@
 package com.pluralsight;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         displayHomeScreenMenu();
     }
-    public static void displayHomeScreenMenu(){
+
+    public static void displayHomeScreenMenu() {
         Scanner scanner = new Scanner(System.in);
         boolean run = true;
-        while (run){
-            System.out.println("=====Welcome to Your Account Ledger");
-            System.out.println("Select an Option");
+        while (run) {
+            System.out.println("=====Welcome to Your Account Ledger=====");
             System.out.println("D) Make a Deposit");
             System.out.println("P) Make a Payment");
             System.out.println("L) View Ledger");
             System.out.println("E) Exit");
-
+            System.out.println("Select an Option: ");
             String choice = scanner.nextLine().toUpperCase();
-            if (choice.equals("D")){
+            if (choice.equals("D")) {
                 addDeposit(scanner);
             } else if (choice.equals("E")) {
                 run = false;
@@ -35,7 +34,8 @@ public class AccountingLedgerApp {
             }
         }
     }
-    public static void addDeposit (Scanner scanner){
+
+    public static void addDeposit(Scanner scanner) {
         System.out.println("Add a New Deposit");
         System.out.println("Enter Date of Deposit (Ex. YYYY/MM/DD)");
         String date = scanner.nextLine();
@@ -49,10 +49,93 @@ public class AccountingLedgerApp {
         String amount = scanner.nextLine();
         String transaction = date + "|" + time + "|" + description + "|" + vendor + "|" + amount + "\n";
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv", true))){
-            
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv", true))) {
+            writer.write(transaction);
+            System.out.println("Your Deposit Has Been Added");
+        } catch (IOException e) {
+            System.out.println("Error, Try Again" + e.getMessage());
         }
-
     }
 
+    public static void makePayment(Scanner scanner) {
+        System.out.println("Make a Payment (Debit)");
+        System.out.println("Payment Date (YYYY/MM/DD");
+        String date = scanner.nextLine();
+        System.out.println("Time of Payment? (Ex. HH:MM:SS)");
+        String time = scanner.nextLine();
+        System.out.println("Description of Payment?");
+        String description = scanner.nextLine();
+        System.out.println("Vendor Name?");
+        String vendor = scanner.nextLine();
+        System.out.println("Payment Amount?");
+        String amount = scanner.nextLine();
+        String transaction = date + "|" + time + "|" + description + "|" + vendor + "|" + amount + "\n";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv", true))) {
+            writer.write(transaction);
+            System.out.println("Your Payment Has Been Added");
+        } catch (IOException e) {
+            System.out.println("Error, Try Again" + e.getMessage());
+        }
+    }
+
+    public static void displayLedgerMenu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        while (run) {
+            System.out.println("Your Ledger Account");
+            System.out.println("Select an Option: ");
+            System.out.println("A) All Deposits");
+            System.out.println("P) All Payments");
+            System.out.println("R) Reports");
+            String choice = scanner.nextLine();
+            if (choice.equals("A")) {
+                displayAllLedgerInfo();
+            } else if (choice.equals("P")) {
+                displayPayments();
+            } else if (choice.equals("R")) {
+                displayReports();
+            }
+        }
+        String choice = scanner.nextLine().toUpperCase();
+    }
+
+    public static void displayAllLedgerInfo() {
+        System.out.println("All Ledger Info");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("transaction.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error, Try again: " + e.getMessage());
+        }
+    }
+
+    public static void displayPayments() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("transaction.csv"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|");
+                if (parts.length == 5) {
+                    String amountStr = parts[4].trim();
+                    try {
+                        double amount = Double.parseDouble(amountStr);
+                        if (amount < 0 ){
+                            System.out.println(line);
+                        }
+                    } catch (NumberFormatException e){
+                        System.out.println("Error" + amountStr + "Line: " + line);
+                    }
+                }
+            }
+        } catch (IOException e){
+            System.out.println("Error, Try Again" + e.getMessage());
+        }
+    }
+    public static void displayReports(){
+        Scanner scanner = new Scanner(System.in);
+    }
 }
+
